b8f3f606886c5295b5cff8b6c62aff4e6c899a9c
Anjani Tharayil <anjani.tharayil@bt.com>
2024-12-12 18:36:06 +0530

    react monorepo

--- apps/inventory/src/app/app.tsx
+++ apps/inventory/src/app/app.tsx
@@ -2,18 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 
 // importing the component from the library
 import { Products } from '@react-monorepo/products';
-import { Orders } from '@react-monorepo/orders';
 
 function Home() {
-  return <h1>Home</h1>;
+  return <h1>Inventory</h1>;
 }
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/products" element={<Products />}></Route>
-      <Route path="/orders" element={<Orders />}></Route>
+      <Route path="/orders" element={<Products />}></Route>
     </Routes>
   );
 }
